39e735df648367ea3c24b4eab45668a36078b572
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-04-02 21:01:43 +0200

    Adding more resources.

--- myproject/testing/ext/_version.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-Copyright (C) 2025, Zato Source s.r.o. https://zato.io
-
-Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
-"""
-
-__version__ = '4.1.20260401212956'
